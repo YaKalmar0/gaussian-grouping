a12e5cdbf64cac373368953b86ea8e87861691dd
Andrii Yakovyna <ayakovyna@itemis.com>
2024-03-26 17:26:39 +0100

    feat: load dataset from s3

--- sage-entry.py
+++ sage-entry.py
@@ -1,11 +1,18 @@
 import subprocess
 import shutil
 import sys
+import os
 
 if "__name__" == "__main__":
     subprocess.call(['conda', 'env', 'create', '-f', 'environment.yml'])
-    subprocess.call(['source', 'activate', 'gaussian_grouping2'])   
+    subprocess.call(['source', 'activate', 'gaussian_grouping2'])
+    data_dir = os.environ["SM_CHANNEL_TRAIN"]
+    os.mkdir('./data')
+    shutil.move(data_dir, './data')
+
+
     print(subprocess.run([sys.executable, "./script/train.sh", "mipnerf/kitchen", "2"]))
     shutil.move('./output', '/opt/ml/output/data')
+    
 
     
