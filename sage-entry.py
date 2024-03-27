99e2f236a02ff12e3295413071162ba08cb2b91f
Andrii Yakovyna <ayakovyna@itemis.com>
2024-03-27 15:40:05 +0100

    test

--- sage-entry.py
+++ sage-entry.py
@@ -6,7 +6,8 @@ import os
 if __name__ == "__main__":
     print('Configuring Conda environment...')
     subprocess.call(['conda', 'create', '-n' 'gaussian_grouping', "python=3.8", '-y'])
-    subprocess.call(['source', 'activate', 'gaussian_grouping'])
+    subprocess.call(['conda', 'init', 'bash'])
+    subprocess.call(['conda', 'activate', 'gaussian_grouping'])
     subprocess.call(['python', '-m', 'pip', 'install', '-r', 'reqs.txt'])
     print('Conda environment successfully configured')
     
